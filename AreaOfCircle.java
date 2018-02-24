92392a71ece7032e4a0ca94a12ee6f8308d11c22
hiramGL <hiram.garcia1@upr.edu>
2018-02-24 16:35:30 -0400

    Revert "Added Perimeter feature"
    This reverts commit d929c0021cdc94143546275259621ee73bd1ee67.

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -3,7 +3,7 @@ public class AreaOfCircle {
 	private static double area;
 	public static void main(String[] args) {
 		areaCalculator(2);
-		System.out.println(area/2);
+		System.out.println(area);
 	}
 	public static void areaCalculator(double radius){
 		 area = Math.PI* Math.pow(radius, 2);
